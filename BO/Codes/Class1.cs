4f832d6d78b51db591a833470814cbb9c1d5f5bf
pandadika <pandadika@gmail.com>
2022-03-01 12:38:39 +0100

    init

---
+++ BO/Codes/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO.Codes
+{
+    internal class Vehicles
+    {
+        public int Horsepower { get; set; }
+    }
+
+    internal class Car : Vehicles
+    {
+        public string Make { get; set; }
+
+    }
+
+    internal class Audi : Car
+    {
+        public string Model { get; set; }
+    }
+}
+
